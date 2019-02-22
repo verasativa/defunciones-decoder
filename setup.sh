1349772e199977d23618fbb940bbfe42986984b2
Vera Sativa <hola@verasativa.com>
2019-02-22 20:57:28 -0300

    feat: added environment setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pip install --upgrade pip
+pip install pandas_access
+pip install tqdm -U
+pip install simpledbf
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+pip install xlrd
+apt update
+apt install mdbtools -Y
+pip install hurry.filesize
+pip install openpyxl
+pip install -U pandas
